89c23c8d4716453e5d38e1d973f4c01dbb1ab597
ayman <aymanjaber2012@gmail.com>
2021-09-01 08:20:10 +0300

    Add users controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,6 +10,18 @@ class ApplicationController < ActionController::Base
 
   private
 
+  def authenticate_user!(options = {})
+    head :unauthorized unless signed_in?
+  end
+
+  def current_user
+    @current_user ||= super || User.find(@current_user_id)
+  end
+
+  def signed_in?
+    @current_user_id.present?
+  end
+
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
   end
