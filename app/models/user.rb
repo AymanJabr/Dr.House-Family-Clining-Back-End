6863a6f0f4eef0eb344388853f206346a9dec12d
ayman <aymanjaber2012@gmail.com>
2021-09-02 13:00:31 +0300

    Add Appointments model

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,8 @@ class User < ApplicationRecord
 
   validates :username, uniqueness: { case_sensitive: false }, presence: true, allow_blank: false, format: { with: /\A[a-zA-Z0-9]+\z/ }
 
+  has_many :appointments
+
   def generate_jwt
     JWT.encode({ id: id,
               exp: 60.days.from_now.to_i },
---
+++ db/migrate/20210902095002_create_appointments.rb
@@ -0,0 +1,10 @@
+class CreateAppointments < ActiveRecord::Migration[6.1]
+  def change
+    create_table :appointments do |t|
+      t.belongs_to :user, index: true, foreign_key: true
+      t.datetime :appointment_begin
+      t.datetime :appointment_end
+      t.timestamps
+    end
+  end
+end
